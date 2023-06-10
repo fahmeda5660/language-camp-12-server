702f9445d54a4240e3af0c17deba725ea9373b06
fahmeda5660 <fariha.208860@gmail.com>
2023-06-10 22:08:30 +0600

    user in database

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express'); 
+const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const jwt = require('jsonwebtoken');
@@ -9,6 +9,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.urfvppf.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,10 +34,10 @@ const client = new MongoClient(uri, {
     strict: true,
     deprecationErrors: true,
   },
-//edited
+  //edited
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  maxPoolSize:10,
+  maxPoolSize: 10,
 });
 
 
@@ -29,12 +45,40 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // edited
-    client.connect((err)=>{
-        if(err){
-          console.error(err);
-          return;
-        }
-      });
+    client.connect((err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
+
+    const usersCollection = client.db("languageDb").collection("users");
+    // JWT
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+      res.send({ token })
+    })
+
+    // User related apis
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      // console.log(user);
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+      // console.log("existingUser",existingUser);
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -48,7 +92,7 @@ run().catch(console.dir);
 
 // create a GET route
 app.get('/', (req, res) => {
-  res.send({ express: 'YOUR Language Camps IS CONNECTED TO REACT' }); 
+  res.send({ express: 'YOUR Language Camps IS CONNECTED TO REACT' });
 });
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`));
