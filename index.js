bfdbbeca7c5cc46e488c411e08a1add44061edd4
Fahmeda Hasan Fariha <58398404+fahmeda5660@users.noreply.github.com>
2023-11-07 00:25:26 +0600

    test

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const app = express();
 const port = process.env.PORT || 5000;
 // middleware
-app.use(cors());
+// app.use(cors());
+const corsConfig = {
+    origin: '',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("", cors(corsConfig))
 app.use(express.json());
 
 const verifyJWT = (req, res, next) => {
